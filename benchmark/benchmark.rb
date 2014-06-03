0ee57b440403a1c7e81e8efbdb97d32b162da931
Vadym Tyemirov <temirov@gmail.com>
2014-06-03 11:53:37 -0700

    banchmark improved

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -1,28 +1,30 @@
 require 'benchmark'
 require_relative '../lru_cache'
 
-Benchmark.bmbm do |r| 
-  max = 400
-  lru_cache = LruCache.new max: max
+Benchmark.bmbm do |r|
+  min = 400
+  max = 4000
+
+  lru_cache = LruCache.new max: min
   key = -> { ("a".."z").to_a.sample }
   value = -> { Random.new.rand(10..100) }
-  r.report "Set keys and values for the cache" do
-    400.times do 
+  r.report "Set keys and values for the cache #{min} times" do
+    min.times do 
       lru_cache.set key.call, value.call 
     end
   end
-  r.report "Get values from cache" do
-    400.times do 
+  r.report "Get values from cache #{min} times" do
+    min.times do 
       lru_cache.get key.call
     end
   end
-  r.report "Set keys and values for the cache" do
-    4000.times do 
+  r.report "Set keys and values for the cache #{max} times" do
+    max.times do 
       lru_cache.set key.call, value.call 
     end
   end
-  r.report "Get values from cache" do
-    4000.times do 
+  r.report "Get values from cache #{max} times" do
+    max.times do 
       lru_cache.get key.call
     end
   end
